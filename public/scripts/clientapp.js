28a131ad62084baa0113581cfd92b1b20a77713a
Brent Reno <brent.reno@gmail.com>
2016-09-26 15:01:11 -0500

    assemble object to send from Dom data

--- public/scripts/clientapp.js
+++ public/scripts/clientapp.js
@@ -1,1 +1,18 @@
 console.log("script is sourced");
+
+var myApp = angular.module("myApp", []);
+
+myApp.controller("heroesController", ["$scope", "$http", function($scope, $http){
+  $scope.addHero = function(){
+    // create hero to send object
+    var heroToSend ={
+      alias: $scope.alias,
+      first_name: $scope.firstName,
+      last_name: $scope.lastName,
+      city: $scope.city,
+      power_name: $scope.superPower
+    };// end heroToSend
+    console.log("this is the hero", heroToSend);
+  };//end addHero
+
+}]);// end heroes controller

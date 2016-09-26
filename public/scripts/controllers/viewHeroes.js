d3989884c47a78cc08b4c2a5572bb78fc2e7673e
Brent Reno <brent.reno@gmail.com>
2016-09-26 15:35:41 -0500

    Display Heroes on Dom

--- public/scripts/controllers/viewHeroes.js
+++ public/scripts/controllers/viewHeroes.js
@@ -1,3 +1,15 @@
 myApp.controller("viewHeroesController", ["$scope", "$http", function($scope, $http){
 console.log("in view Heroes");
+
+//get heroes
+var viewHeroes= function(){
+    $http({
+      method: 'GET',
+      url: '/heroes'
+    }).then(function ( response ){
+      console.log('back from server with:', response.data);
+      $scope.allHeroes =response.data;
+    });// end http Call
+  };// end view heroes
+  viewHeroes();
 }]);
